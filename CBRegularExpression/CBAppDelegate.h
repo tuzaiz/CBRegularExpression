608a38c410e2d69d16287b0c2a407e9145327d33
Henry Tseng <rabbit.henry@gmail.com>
2014-03-12 00:32:20 +0800

    Initial Commit

---
+++ CBRegularExpression/CBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CBAppDelegate.h
+//  CBRegularExpression
+//
+//  Created by Henry on 3/12/14.
+//  Copyright (c) 2014 Cloudbay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CBRegularExpression/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CBRegularExpression
+//
+//  Created by Henry on 3/12/14.
+//  Copyright (c) 2014 Cloudbay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CBAppDelegate class]));
+    }
+}
